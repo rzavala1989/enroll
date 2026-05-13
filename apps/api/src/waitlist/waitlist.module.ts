b7ab06a20d8b209a6956539ae44e09dd8eaf58fc
Ricardo Zavala <zavala.ricardo.m@gmail.com>
2026-05-12 17:42:19 -0700

    waitlist: import AuthModule so controller guards resolve JwtService

--- apps/api/src/waitlist/waitlist.module.ts
+++ apps/api/src/waitlist/waitlist.module.ts
@@ -2,12 +2,17 @@ import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
 
 import { AuditModule } from '../audit/audit.module';
+import { AuthModule } from '../auth/auth.module';
 import { WaitlistController } from './waitlist.controller';
 import { WaitlistProcessor } from './waitlist.processor';
 import { PROMOTE_WAITLIST_QUEUE, WaitlistService } from './waitlist.service';
 
 @Module({
-  imports: [AuditModule, BullModule.registerQueue({ name: PROMOTE_WAITLIST_QUEUE })],
+  imports: [
+    AuditModule,
+    AuthModule,
+    BullModule.registerQueue({ name: PROMOTE_WAITLIST_QUEUE }),
+  ],
   controllers: [WaitlistController],
   providers: [WaitlistService, WaitlistProcessor],
   exports: [WaitlistService],
