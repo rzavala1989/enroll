c806ca7f82875894037b526b1f50a82922dfe948
Ricardo Zavala <zavala.ricardo.m@gmail.com>
2026-05-12 17:29:07 -0700

    wire BullMQ root module from REDIS_URL

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,5 +1,6 @@
+import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AuditModule } from './audit/audit.module';
@@ -8,17 +9,36 @@ import { CoursesModule } from './courses/courses.module';
 import { EnrollmentModule } from './enrollment/enrollment.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { WaitlistModule } from './waitlist/waitlist.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     ScheduleModule.forRoot(),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const url = new URL(config.getOrThrow<string>('REDIS_URL'));
+        return {
+          connection: {
+            host: url.hostname,
+            port: url.port ? Number(url.port) : 6379,
+            username: url.username || undefined,
+            password: url.password || undefined,
+            maxRetriesPerRequest: null,
+            ...(url.protocol === 'rediss:' ? { tls: {} } : {}),
+          },
+        };
+      },
+    }),
     PrismaModule,
     AuthModule,
     UsersModule,
     CoursesModule,
     AuditModule,
     EnrollmentModule,
+    WaitlistModule,
   ],
   controllers: [AppController],
 })
